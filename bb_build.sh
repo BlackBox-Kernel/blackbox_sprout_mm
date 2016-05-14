f4472ef7eef0c5e39458cfef980e05cbf50e3c6c
Kunal Kene <kunalkene1797@gmail.com>
2016-05-14 20:32:38 +0530

    Add BlackBox Build Script
    for faster kernel compilation

---
+++ bb_build.sh
@@ -0,0 +1,66 @@
+#
+# Copyright © 2016, Kunal Kene "kunalkene1797" <kunalkene1797@gmail.com>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+
+# Init Script
+KERNEL_DIR=$PWD
+ZIMAGE=$KERNEL_DIR/arch/arm/boot/zImage
+BUILD_START=$(date +"%s")
+
+# Color Code Script
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+nocol='\033[0m'         # Default
+
+# Tweakable Options Below
+export ARCH=arm
+export SUBARCH=arm
+export KBUILD_BUILD_USER="KunalKene1797"
+export KBUILD_BUILD_HOST="BlackServer"
+#export CROSS_COMPILE="arm-eabi-6.0/bin/arm-eabi-"
+
+# Compilation Scripts Are Below
+compile_kernel ()
+{
+echo -e "$White***********************************************"
+echo "         Compiling BlackBox kernel             "
+echo -e "***********************************************$nocol"
+make clean && make mrproper
+make cyanogenmod_sprout_defconfig
+make -j4
+if ! [ -a $ZIMAGE ];
+then
+echo -e "$Red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
+fi
+}
+
+# Finalizing Script Below
+case $1 in
+clean)
+make ARCH=arm -j3 clean mrproper
+rm -rf include/linux/autoconf.h
+;;
+*)
+compile_kernel
+;;
+esac
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$Yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
